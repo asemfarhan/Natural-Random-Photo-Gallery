80a94e82f4f872add323e77f725f35399e3fd588
asemfarhan <asem.mohammed.f@gmail.com>
2019-11-17 21:47:38 +0300

    Updates

--- precache-manifest.28b7d71467a3ac364791468d2821a638.js
+++ precache-manifest.28b7d71467a3ac364791468d2821a638.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "637bb2bb3e4c9bcecd5a997a33d764ee",
+    "revision": "e839945787f0dca6c1f245298bd2f087",
     "url": "/Natural-Random-Photo-Gallery/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Natural-Random-Photo-Gallery/static/css/2.eb45ad44.chunk.css"
   },
   {
-    "revision": "d1054b1cc9938489df56",
+    "revision": "45e4126bf4a4a629ff49",
     "url": "/Natural-Random-Photo-Gallery/static/css/main.c06035db.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Natural-Random-Photo-Gallery/static/js/2.d2b90d07.chunk.js"
   },
   {
-    "revision": "d1054b1cc9938489df56",
-    "url": "/Natural-Random-Photo-Gallery/static/js/main.e35e5811.chunk.js"
+    "revision": "45e4126bf4a4a629ff49",
+    "url": "/Natural-Random-Photo-Gallery/static/js/main.4d413e2d.chunk.js"
   },
   {
     "revision": "7ee06b4b748a0fdd3f5c",
