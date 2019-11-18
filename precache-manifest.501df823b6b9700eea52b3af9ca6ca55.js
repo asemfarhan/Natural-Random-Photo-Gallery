388fe0b3e9e64df04066bcb207c970d5a3feac3c
asemfarhan <asem.mohammed.f@gmail.com>
2019-11-18 04:27:02 +0300

    Updates

--- precache-manifest.501df823b6b9700eea52b3af9ca6ca55.js
+++ precache-manifest.501df823b6b9700eea52b3af9ca6ca55.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "637bb2bb3e4c9bcecd5a997a33d764ee",
+    "revision": "d09ccb3efdacab5c87f2361245e77294",
     "url": "/Natural-Random-Photo-Gallery/index.html"
   },
   {
-    "revision": "d52d2ffb26d3e94bef30",
+    "revision": "884cbdda3bacea0a9c46",
     "url": "/Natural-Random-Photo-Gallery/static/css/2.eb45ad44.chunk.css"
   },
   {
-    "revision": "d1054b1cc9938489df56",
+    "revision": "119f963377582150b09c",
     "url": "/Natural-Random-Photo-Gallery/static/css/main.c06035db.chunk.css"
   },
   {
-    "revision": "d52d2ffb26d3e94bef30",
-    "url": "/Natural-Random-Photo-Gallery/static/js/2.d2b90d07.chunk.js"
+    "revision": "884cbdda3bacea0a9c46",
+    "url": "/Natural-Random-Photo-Gallery/static/js/2.cade5617.chunk.js"
   },
   {
-    "revision": "d1054b1cc9938489df56",
-    "url": "/Natural-Random-Photo-Gallery/static/js/main.e35e5811.chunk.js"
+    "revision": "119f963377582150b09c",
+    "url": "/Natural-Random-Photo-Gallery/static/js/main.2703f573.chunk.js"
   },
   {
     "revision": "7ee06b4b748a0fdd3f5c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Natural-Random-Photo-Gallery/precache-manifest.f3e02dbebee7a82dbf5f41ce2a92a906.js"
+  "/Natural-Random-Photo-Gallery/precache-manifest.501df823b6b9700eea52b3af9ca6ca55.js"
 );
 
 self.addEventListener('message', (event) => {
